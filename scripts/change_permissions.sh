21c58ff8fa07fcea00a848d33f52c697e2dbc77a
EC2 Default User <ec2-user@ip-172-31-88-1.ec2.internal>
2018-06-25 00:12:08 +0000

    update script

--- scripts/change_permissions.sh
+++ scripts/change_permissions.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-cp /wp-upload/wp-config.php /WordPress/releases/20180623/
-cd /WordPress/releases/20180623/wp-content
+DIR=$1
+cp /wp-upload/wp-config.php ${DIR}
+cd ${DIR}/wp-content
 ln -s /wp-upload/uploads uploads
 cd /var/www
 #rm html
-ln -s /WordPress/releases/20180623 html
-chown apache:apache -R /WordPress/releases/20180623
+ln -s ${DIR} html
+chown apache:apache -R ${DIR} 
